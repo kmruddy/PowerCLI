3cb6c838d6168f375cf40504a9a17f5811433bea
Kyle Ruddy <kmruddy@users.noreply.github.com>
2016-11-22 16:30:43 -0500

    Create Update_PowerCLI_Scripts.ps1
    Created 3 advanced functions: 
    Get-PowerCLIInitializations
    Update-PowerCLIInitializations
    Get-PowerCLISnapinUse
    
    They will search through directories to find where the PowerCLI Initialize Environment script or add-pssnapin VMware.* are being used. The Update function will update the directory being used to reference the initialize script.

---
+++ Update_PowerCLI_Scripts.ps1
@@ -0,0 +1,162 @@
+function Get-PowerCLIInitializations {
+<#  
+.SYNOPSIS  
+    Gathers information on PowerShell resources which refer to the old PowerCLI Initialization Script
+.DESCRIPTION 
+    Will provide an inventory of scripts, modules, etc refering to the old vSphere PowerCLI directory
+.NOTES  
+    Author:  Kyle Ruddy, @kmruddy, thatcouldbeaproblem.com
+.PARAMETER Path
+    Directory path to be searched
+.EXAMPLE
+	Get-PowerCLIInitializations -Path C:\Temp\Scripts
+    Gathers information from the 'C:\Temp\Scripts\' directory 
+#>
+[CmdletBinding()] 
+	param(
+		[Parameter(Mandatory=$false,Position=0,ValueFromPipelineByPropertyName=$true)]
+        [string]$Path
+  	)
+
+	Process {
+
+        #Validate whether a path was passed as a parameter or not
+        if (!$Path) {$FullName = (Get-Location).ProviderPath}
+        else {
+            
+            #Validate whether the path passed as a parameter was a literal path or not, then establish the FullName of the desired path
+            if ((Test-Path -Path $Path -ErrorAction SilentlyContinue) -eq $true -and $Path -is [System.IO.DirectoryInfo]) {$FullName = (Get-Item -Path $Path).FullName}
+            elseif ((Test-Path -Path $Path -ErrorAction SilentlyContinue) -eq $true -and $Path -isnot [System.IO.DirectoryInfo]) {$FullName = (Get-Item -LiteralPath $Path).FullName}
+            else {
+                $currdir = (Get-Location).ProviderPath
+                Write-Warning "No valid path found at - $currdir\$Path"
+            }
+
+        }
+
+        if ($FullName) {
+    
+            #Gather scripts using ps1 extension and have a string matching "\VMware\Infrastructure\vSphere PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1"
+            $scripts = Get-ChildItem -Path $FullName -Recurse -Filter *.ps1 -File -WarningAction SilentlyContinue -ErrorAction SilentlyContinue | Select-String -Pattern "\\VMware\\Infrastructure\\vSphere PowerCLI\\Scripts\\Initialize-PowerCLIEnvironment.ps1"
+
+            #Create a report of neccessary output based on the scripts gathered above
+            $scriptreport = @()
+            foreach ($script in $scripts) {
+
+                $singleitem = New-Object System.Object
+                $singleitem | Add-Member -Type NoteProperty -Name Filename -Value $script.Filename
+                $singleitem | Add-Member -Type NoteProperty -Name LineNumber -Value $script.LineNumber
+                $singleitem | Add-Member -Type NoteProperty -Name Path -Value $script.Path.TrimEnd($script.Filename)
+                $singleitem | Add-Member -Type NoteProperty -Name FullPath -Value $script.Path
+                $scriptreport += $singleitem
+
+            }
+            return $scriptreport
+
+        }
+
+    } # End of process
+} # End of function
+
+function Get-PowerCLISnapinUse {
+<#  
+.SYNOPSIS  
+    Gathers information on PowerShell resources which refer to the old PowerCLI Snapins
+.DESCRIPTION 
+    Will provide an inventory of scripts, modules, etc refering to the old PowerCLI Snapins
+.NOTES  
+    Author:  Kyle Ruddy, @kmruddy, thatcouldbeaproblem.com
+.PARAMETER Path
+    Directory path to be searched
+.EXAMPLE
+	Get-PowerCLISnapinUse -Path C:\Temp\Scripts
+    Gathers information from the 'C:\Temp\Scripts\' directory 
+#>
+[CmdletBinding()] 
+	param(
+		[Parameter(Mandatory=$false,Position=0,ValueFromPipelineByPropertyName=$true)]
+        [string]$Path
+  	)
+
+	Process {
+
+        #Validate whether a path was passed as a parameter or not
+        if (!$Path) {$FullName = (Get-Location).ProviderPath}
+        else {
+        
+            #Validate whether the path passed as a parameter was a literal path or not, then establish the FullName of the desired path
+            if ((Test-Path -Path $Path -ErrorAction SilentlyContinue) -eq $true -and $Path -is [System.IO.DirectoryInfo]) {$FullName = (Get-Item -Path $Path).FullName}
+            elseif ((Test-Path -Path $Path -ErrorAction SilentlyContinue) -eq $true -and $Path -isnot [System.IO.DirectoryInfo]) {$FullName = (Get-Item -LiteralPath $Path).FullName}
+            else {
+                $currdir = (Get-Location).ProviderPath
+                Write-Warning "No valid path found at - $currdir\$Path"
+            }
+
+        }
+
+        if ($FullName) {
+    
+            #Gather scripts using ps1 extension and have a string matching "add-pssnapin" and "VMware." on the same line
+            $scripts = Get-ChildItem -Path $FullName -Recurse -Filter *.ps1 -File -WarningAction SilentlyContinue -ErrorAction SilentlyContinue | Select-String -Pattern "add-pssnapin" | Where-Object {$_ | Select-String -Pattern "VMware."}
+
+            #Create a report of neccessary output based on the scripts gathered above
+            $scriptreport = @()
+            foreach ($script in $scripts) {
+
+                $singleitem = New-Object System.Object
+                $singleitem | Add-Member -Type NoteProperty -Name Filename -Value $script.Filename
+                $singleitem | Add-Member -Type NoteProperty -Name LineNumber -Value $script.LineNumber
+                $singleitem | Add-Member -Type NoteProperty -Name Path -Value $script.Path.TrimEnd($script.Filename)
+                $singleitem | Add-Member -Type NoteProperty -Name FullPath -Value $script.Path
+                $scriptreport += $singleitem
+
+            }
+
+            return $scriptreport
+
+        }
+
+    } # End of process
+} # End of function
+
+function Update-PowerCLIInitializations {
+<#  
+.SYNOPSIS  
+    Updates the information in PowerShell resources which refer to the old PowerCLI Initialization Script
+.DESCRIPTION 
+    Will update scripts, modules, etc refering to the old vSphere PowerCLI directory
+.NOTES  
+    Author:  Kyle Ruddy, @kmruddy, thatcouldbeaproblem.com
+.PARAMETER Path
+    Directory path to be searched
+.EXAMPLE
+	Update-PowerCLIInitializations -Path C:\Temp\Scripts
+    Gathers information from the 'C:\Temp\Scripts\' directory 
+#>
+[CmdletBinding()] 
+	param(
+		[Parameter(Mandatory=$false,Position=0,ValueFromPipelineByPropertyName=$true)]
+        [string]$Path
+  	)
+
+	Process {
+
+        #Gather scripts using ps1 extension and have a string matching "\VMware\Infrastructure\vSphere PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1"
+        $scripts = Get-PowerCLIInitializations -Path $Path
+
+        #Check to see if any scripts are found
+        if (!$scripts) {Write-Warning "No PowerShell resources found requiring update within $Path"}
+        else {
+
+            foreach ($script in $scripts) {
+
+                #Finds and updates the string to the new location of the Initialize-PowerCLIEnvironment.ps1 script
+                (Get-Content $script.FullPath).Replace("\VMware\Infrastructure\vSphere PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1","\VMware\Infrastructure\PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1") | Set-Content $script.FullPath
+
+            }
+
+        }
+
+
+    } # End of process
+} # End of function
