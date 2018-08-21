834299c9bd497c2e8939c313e2681238fb315a31
Kyle Ruddy <kruddy@vmware.com>
2018-08-21 15:37:34 -0400

    Updating 1 Host Scipt
    Add the ability to skip the AWS Account association.

--- powercli_1node.ps1
+++ powercli_1node.ps1
@@ -10,6 +10,7 @@ $oauthToken = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx"
 $sddcName = "PowerCLI-1Node-SDDC"
 $hostCount = "1"
 $awsRegion = "US_WEST_2"
+$useAwsAccount = $false
 
 # --- Deployment code  ---
 # Connect to VMware Cloud Service
@@ -20,17 +21,20 @@ $orgSvc = Get-VmcService -Name com.vmware.vmc.orgs
 $org = $orgSvc.List()
 Write-Host "Org:"$org.display_name" ID:"$org.id
 
-# Get Linked Account ID
-$connAcctSvc = Get-VmcService -Name com.vmware.vmc.orgs.account_link.connected_accounts
-$connAcctId = $connAcctSvc.get($org.id) | Select-Object -ExpandProperty id
-Write-Host "Account ID: $connAcctId"
+# Check to use the already existing AWS account connection
+if ($useAwsAccount -eq $true) {
+    # Get Linked Account ID
+    $connAcctSvc = Get-VmcService -Name com.vmware.vmc.orgs.account_link.connected_accounts
+    $connAcctId = $connAcctSvc.get($org.id) | Select-Object -ExpandProperty id
+    Write-Host "Account ID: $connAcctId"
 
-# Get Subnet ID
-$compSubnetSvc = Get-VmcService -Name com.vmware.vmc.orgs.account_link.compatible_subnets
-$vpcMap = $compSubnetSvc.Get($org.id, $connAcctId, $region) | Select-Object -ExpandProperty vpc_map 
-$compSubnets = $vpcMap | Select-Object -ExpandProperty Values | Select-Object -ExpandProperty subnets
-$compSubnet = $compSubnets | where {$_.name -ne $null} | Select-Object -first 1
-Write-Host "Subnet CIDR"$compSubnet.subnet_cidr_block"ID:"$compSubnet.subnet_id
+    # Get Subnet ID
+    $compSubnetSvc = Get-VmcService -Name com.vmware.vmc.orgs.account_link.compatible_subnets
+    $vpcMap = $compSubnetSvc.Get($org.id, $connAcctId, $region) | Select-Object -ExpandProperty vpc_map 
+    $compSubnets = $vpcMap | Select-Object -ExpandProperty Values | Select-Object -ExpandProperty subnets
+    $compSubnet = $compSubnets | where {$_.name -ne $null} | Select-Object -first 1
+    Write-Host "Subnet CIDR"$compSubnet.subnet_cidr_block"ID:"$compSubnet.subnet_id
+}
 
 # Deploy the SDDC
 $sddcSvc = Get-VmcService com.vmware.vmc.orgs.sddcs
@@ -40,12 +44,20 @@ $sddcCreateSpec.Name = $sddcName
 $sddcCreateSpec.num_hosts = $hostCount
 if ($org.properties.values.sddcTypes) {$sddcCreateSpec.sddc_type = "1NODE"}
 $sddcCreateSpec.Provider = "AWS"
-$accountLinkSpec = $sddcSvc.Help.create.sddc_config.account_link_sddc_config.Element.Create()
-$accountLinkSpec.connected_account_id = $connAcctId
-$custSubId0 = $sddcSvc.Help.create.sddc_config.account_link_sddc_config.Element.customer_subnet_ids.Element.Create()
-$custSubId0 = $compSubnet.subnet_id
-$accountLinkSpec.customer_subnet_ids.Add($custSubId0) | Out-Null
-$sddcCreateSpec.account_link_sddc_config.Add($accountLinkSpec) | Out-Null
-$sddcCreateSpec
+
+if ($useAwsAccount -eq $true) {
+    $accountLinkSpec = $sddcSvc.Help.create.sddc_config.account_link_sddc_config.Element.Create()
+    $accountLinkSpec.connected_account_id = $connAcctId
+    $custSubId0 = $sddcSvc.Help.create.sddc_config.account_link_sddc_config.Element.customer_subnet_ids.Element.Create()
+    $custSubId0 = $compSubnet.subnet_id
+    $accountLinkSpec.customer_subnet_ids.Add($custSubId0) | Out-Null
+    $sddcCreateSpec.account_link_sddc_config.Add($accountLinkSpec) | Out-Null
+}
+elseif ($useAwsAccount -eq $false) {
+    $accountLinkDelaySpec = $sddcSvc.Help.create.sddc_config.account_link_config.delay_account_link.Create()
+    $accountLinkDelaySpec = $true
+    $sddcCreateSpec.account_link_config.delay_account_link = $accountLinkDelaySpec
+}
+
 $newSddc = $sddcSvc.create($org.Id, $sddcCreateSpec)
 $newSddc
